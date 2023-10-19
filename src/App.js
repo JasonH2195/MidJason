f1353dc2c67b89eac626dc0091106702db14e2a3
Jason Hui <jason.ks.hui@hotmail.com>
2023-10-19 17:12:27 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import {Button} from 'react-bootstrap';
 
 function App() {
+
+  const [name,setName] =useState("");
+  const [description,setDescription] =useState("");
+  const [catergory,setCatergory] =useState("");
+  const [quantity,setQuantity] =useState("");
+  const [price,setPrice] =useState("");
+
+
+  const handleName = (e) => setName(e.target.value);   
+  const handleDescription = (e) => setDescription(e.target.value);   
+  const handleCatergory = (e) => setCatergory(e.target.value);   
+  const handleQuantity = (e) => setQuantity(e.target.value);   
+  const handlePrice = (e) => setPrice(e.target.value);   
+
+  const handleSubmit = (e) => {
+    alert(`Name: ${name}
+           Description: ${description}
+           Catergory:${catergory}
+           Quantity:${quantity}
+           Price:${price}
+        `)
+  }
+
+  const handleCancel = (e) => {
+    setName(""); 
+    setDescription(""); 
+    setCatergory(""); 
+    setQuantity(""); 
+    setPrice(""); 
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1> New product</h1>
+      <form>
+        <table className="table"> 
+          <tbody>
+            <tr>
+              <td>
+                <input type="text" value={name} onChange={handleName} placeholder='Name' style={{width:"30%"}}/> 
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <input type="text" value={description} onChange={handleDescription} placeholder='Description' style={{width:"30%"}}/> 
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <input type="text" value={catergory} onChange={handleCatergory} placeholder='Catergory' style={{width:"30%"}}/> 
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <input type="text" value={quantity} onChange={handleQuantity} placeholder='Quantity' style={{width:"30%"}}/> 
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <input type="text" value={price} onChange={handlePrice} placeholder='Price' style={{width:"30%"}} /> 
+              </td>
+            </tr>
+            <tr>  
+              <td>
+                <table className="table">
+                  <tbody>
+                    <tr>
+                      <td>
+                        <Button size="sm" variant='secondary' onClick={handleSubmit}>SUBMIT</Button>
+                      </td>
+                      <td>
+                        <Button size="sm" variant='secondary' onClick={handleCancel}>CANCEL</Button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>                
+              </td>
+            </tr>
+
+          </tbody>
+        </table>
+
+      </form>
+
+
+
+
+
+
     </div>
   );
 }
